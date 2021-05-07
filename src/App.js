bd3f04a039d87711300d580922372a7a6f36bee8
Nikolay-Biryukov <nbiryukov@kameleoon.com>
2021-05-07 19:16:03 +0300

    context start

--- src/App.js
+++ src/App.js
@@ -5,39 +5,36 @@ import Map from './Map.jsx';
 import Profile from './Profile.jsx';
 import Login from './Login.jsx';
 import Registration from './Registration.jsx';
+import { widthAuth } from './AuthContext';
 
-const getPage = (page, callback) => {
-
-  switch (page) {
-    case 'map':
-      return <Map />;
-    case 'profile':
-      return <Profile />
-    case 'login':
-      return <Login onSubmit={callback}/>;
-    case 'registration':
-      return <Registration onSubmit={callback}/>
-    default:
-      return <Map />;
-    }
+const PAGES = {
+  'map': (props) => <Map {...props}/>,
+  'profile': (props) => <Profile {...props} />,
+  'login': (props) => <Login {...props} />,
+  'registration': (props) => <Registration {...props} />
 }
 
 class App extends React.Component {  
 
-  state = { page: 'login'}
+  state = { page: 'registration'}
 
   goToPage = (page) => {
+    if (this.props.isLoggedIn) {
+      this.setState({ page })
+      return
+    }
     this.setState({ page })
+    //this.setState({page: 'login'})
   };
 
   render() {
     return <div className="App">
       <Header goToPage={this.goToPage} />
  
-      {getPage(this.state.page, this.goToPage)}
+      {PAGES[this.state.page]({onSubmit: this.goToPage})}
     </div>
   };
 }
 
 
-export default App;
+export default widthAuth(App);
--- src/Map.jsx
+++ src/Map.jsx
@@ -1,3 +1,28 @@
-const Map = () => (<div>Page  Map</div>)
+import React from 'react';
+import mapboxgl from 'mapbox-gl';
 
-export default Map;
+export default class Map extends React.Component {
+    map = null;
+    mapContainer = React.createRef();
+
+    componentDidMount() {
+      mapboxgl.accessToken = 'pk.eyJ1Ijoibmlrb2xheWJpcnl1a292IiwiYSI6ImNrb2JoanRpNzJ6Z2Yyb2x5em5pZmQ1c24ifQ.9joEFmwELRAyd9kxij0j_g';
+
+      this.map = new mapboxgl.Map({
+        container: this.mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [37.512602, 55.972004], // starting position
+        zoom: 14
+      });
+    }
+  
+    componentWillUnmount() {
+      this.map.remove();
+    }
+  
+    render() {
+      return <div className="map-wrapper">
+                <div data-testid="map" className="map" ref={this.mapContainer} />
+            </div>;
+    }
+}
